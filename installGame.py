05a91026c8cc7d945c85834150685ae3a5ac4798
Shob3r <andrewpinter23@gmail.com>
2023-11-11 16:08:51 -0800

    Install method now works, hooray!

--- installGame.py
+++ installGame.py
@@ -3,7 +3,8 @@ import os
 
 import requests
 from tqdm import tqdm
-import zipfile
+from clear_screen import clear
+import shutil
 from latestBuild import GetLatestBuildNumber
 
 latestBuildNum = GetLatestBuildNumber("Mission-Monkey")
@@ -12,29 +13,35 @@ gameDirectory = None
 
 # I am too lazy to import from main and whatever so just run the if statement again but instead of the root dir it's the game dir
 if platform.system() != "Windows":
-    gameDirectory = os.path.join(os.path.expanduser("~"), "Mission-Monkey/Game")
+    gameDirectory = os.path.join(os.path.expanduser("~"), "Mission-Monkey")
 else:
-    gameDirectory = os.path.join(os.environ['USERPROFILE'], "/Mission-Monkey/Game")
+    gameDirectory = os.path.join(os.environ['USERPROFILE'], "Mission-Monkey")
 
 
 def installGame():
-    # This entire download code was stol- I mean borrowed from StackOverflow (https://stackoverflow.com/questions/37573483/progress-bar-while-download-file-over-http-with-requests)
-    downloadURL = f"https://github.com/Mission-Monkey/releases/{latestBuildNum}/{latestBuildNum}-{clientPlatform}.zip"
+    # Part of this download code was borrowed from StackOverflow (https://stackoverflow.com/questions/37573483/progress-bar-while-download-file-over-http-with-requests)
+    downloadURL = f"https://github.com/lemons-studios/Mission-Monkey/releases/download/{latestBuildNum}/{latestBuildNum}-{clientPlatform}.zip"
     response = requests.get(downloadURL, stream=True)
     totalSize = int(response.headers.get('content-length'), 0)
     blockSize = 1024  # 1 Kilobyte
 
     progressBar = tqdm(total=totalSize, unit="iB", unit_scale=True)
-    with open('game.zip', 'wb') as archivedGame:
+    with open(os.path.join(gameDirectory, "game.zip"), 'wb') as archivedGame:
         for data in response.iter_content(blockSize):
             progressBar.update(len(data))
             archivedGame.write(data)
     progressBar.close()
     if totalSize != 0 and progressBar.n != totalSize:
-        print("Something really bad happened. Try downloading again?")
+        print("Something terrible happened. Try downloading again?")
 
-    try:
-        with zipfile.ZipFile(os.path.join(gameDirectory, f'/{latestBuildNum}-{clientPlatform}.zip')) as extractor:
-            extractor.extractall()
-    except zipfile.BadZipfile:
-        print("Error: Downloaded zip file is corrupted, please try again")
+    print("Extracing game")
+    gameData = None
+    if platform.system() != 'Windows':
+        gameData = os.path.join(os.environ['USERPROFILE'], "Mission-Monkey", "game.zip")
+        pass
+    else:
+        gameData = os.path.join(os.path.expanduser("~"), "Mission-Monkey", "game.zip")
+        pass
+    shutil.unpack_archive(gameData, gameDirectory)
+    os.remove(gameData)  # Delete the game zip archive after downloading
+    clear()
--- mainMenu.py
+++ mainMenu.py
@@ -1,40 +1,47 @@
 from prompt_toolkit import prompt
 from prompt_toolkit import print_formatted_text as printMenu
 from latestBuild import GetLatestBuildNumber
-
+import installGame
 def menu():
     # Some help from mister GPT for making the menu code but the rest is mine
     printMenu(f"Welcome to the Mission: Monkey cross-platform installer! (Version 0.1)")
     printMenu("Select what you would like to do:")
-    printMenu("1. Install Mission: Monkey")
-    printMenu("2. Check for updates")
-    printMenu("3. Settings")
-    printMenu("4. Quit")
+    printMenu("1. Launch Mission: Monkey")
+    printMenu("2. Install Mission: Monkey")
+    printMenu("3. Check for updates")
+    printMenu("4. Settings")
+    printMenu("5. Quit")
 
     while True:
         try:
-            choice = int(prompt("Select an options (1-4:) "))
-            if 0 <= choice <= 4:  # PyCharm decided to simplify my if statement like this
+            choice = int(prompt("Select an options (1-5:) "))
+            if 0 <= choice <= 5:  # PyCharm decided to simplify my if statement like this
                 break
             else:
-                printMenu("Choose an option between 1 and 4!")
+                printMenu("Choose an option between 1 and 5!")
         except ValueError:
             printMenu("The input you entered is not valid!")
 
     match choice:  # I love swi- I mean match cases!!
         case 1:
-            # Calls the installation method
+            # Launches game if it exists
 
             pass
         case 2:
+            # Calls the installation method
+
+            print(f"Installing Mission Monkey {GetLatestBuildNumber("Mission-Monkey")}.....")
+            installGame.installGame()
+            menu()
+        case 3:
             # Checks for any updates by comparing the latest GitHub tag to the build number for the installation
 
-            pass
-        case 3:
-            # Opens a new menu for settings
 
             pass
         case 4:
+            # Opens a new menu for settings
+            pass
+        case 5:
             # Quits the installer
             printMenu("Goodbye.")
             exit(0)
